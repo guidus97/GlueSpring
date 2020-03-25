6c872adf67233dc68197c00baf5bb110a3aabb34
Guido <guidoruggieri97@gmail.com>
2020-03-25 11:04:29 +0100

    Main Spring Boot application
    Inizialization of SpringBootApplication in the main class

---
+++ src/main/java/com/example/GlueSpring/GlueSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.GlueSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlueSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlueSpringApplication.class, args);
+	}
+
+}
