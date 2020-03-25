6f273c7b6a63ddb1412b40357d062d3868fc616e
Guido <guidoruggieri97@gmail.com>
2020-03-25 11:08:16 +0100

    Other files inizialized
    All the files generated from Spring Inizializr are ready

---
+++ src/test/java/com/example/GlueSpring/GlueSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GlueSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlueSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
