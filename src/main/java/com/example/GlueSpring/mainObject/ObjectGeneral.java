b81c2e7d359a585c5a4bb1fac8017687772b2040
Guido <guidoruggieri97@gmail.com>
2020-03-25 18:56:51 +0100

    Inizialization of parameters
    Created the main ObjectGeneral.java class and the relative app.properties file

---
+++ src/main/java/com/example/GlueSpring/mainObject/ObjectGeneral.java
@@ -0,0 +1,14 @@
+package com.example.GlueSpring.mainObject;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ObjectGeneral {
+
+	public static String mpxUser = "";
+	public static String mpxPass = "";
+	public static String mpxToken = "";
+	public static String mpxSignInUrl = "";
+	public static String mpxStations = "";
+	public static String mpxListing = "";
+}
