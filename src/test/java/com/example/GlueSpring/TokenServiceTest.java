74cfecffe28b4acfc2b8d069a97d9930914a63e2
Guido <guidoruggieri97@gmail.com>
2020-04-01 22:14:52 +0200

    Modified
    Class fixed

--- src/test/java/com/example/GlueSpring/TokenServiceTest.java
+++ src/test/java/com/example/GlueSpring/TokenServiceTest.java
@@ -3,10 +3,12 @@ package com.example.GlueSpring;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
 
 import com.example.GlueSpring.controller.TokenController;
