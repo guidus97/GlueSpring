a126d71882b6ffd7edbb3d2a02ccc1bacd5fda88
Guido <guidoruggieri97@gmail.com>
2020-04-01 21:46:42 +0200

    JUnit 4 support
    Defined test case

---
+++ src/test/java/com/example/GlueSpring/TokenServiceTest.java
@@ -0,0 +1,35 @@
+package com.example.GlueSpring;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import com.example.GlueSpring.service.TokenService;
+
+public class TokenServiceTest {
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	@Autowired
+	private TokenService tokenService;
+	
+	@LocalServerPort
+	private int localServerPort;
+	
+	@Test
+	public void getTokenTest() {
+		
+		ResponseEntity<String> response = tokenService.getToken();
+		ResponseEntity<String> testResponse = restTemplate.getForEntity("http://localhost:" + localServerPort + "/token/getToken", String.class);
+		
+		assertTrue(response.getHeaders().containsKey("Authorization"));
+		assertEquals(response.getStatusCodeValue(), 200);
+		assertTrue(response.getBody().contains("token"));
+		assertEquals(testResponse, response);
+	}
+}
